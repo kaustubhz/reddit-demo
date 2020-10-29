3d84b944883cd6ab8f2473255611ff6d551815c2
kaustubhz <kaustubhz@cdac.in>
2020-10-29 19:00:07 +0530

    Bug fix: Replaced faulty @Transactional with right one

--- RedditBackEnd/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ RedditBackEnd/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -4,33 +4,36 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.Collections;
 
-import javax.transaction.Transactional;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private final UserRepository repository;
 
 	@Override
-	@Transactional()
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String userName) {
+
+		log.info("Inside loadUserByUsername {}",userName);
 
 		Optional<User> userOptional = repository.findByUserName(userName);
 		User user = userOptional
-				.orElseThrow(() -> new UsernameNotFoundException("No user found" + "with the username " + userName));
+				.orElseThrow(() -> new UsernameNotFoundException("No user found" + " with the username " + userName));
 		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getUserPassword(),
 				user.isEnabled(), true, true, true, getAuthorities("USER"));
 	}
