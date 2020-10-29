627a7a98ec1ebb7cb9a8aeb0a6751434315a0439
kaustubhz <kaustubhz@cdac.in>
2020-10-29 18:57:45 +0530

    Bug fix: added missing parameter userDetails in UserPasswordAuthentication class

--- RedditBackEnd/src/main/java/com/example/demo/security/JwtAuthFilter.java
+++ RedditBackEnd/src/main/java/com/example/demo/security/JwtAuthFilter.java
@@ -7,6 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,16 +20,36 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+	@Autowired
+	private JwtProvider jwtProvider;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 //	This method will be useful for scanning HTTP header and extracting JWT token
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		String jwt = getJwtRequest(request);
+		String jwt = getJwtFromRequest(request);
+		if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
 
+			String username = jwtProvider.getUsernameFromJwt(jwt);
+			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+//			Forgot to add userDetails in parameter , caused 403 error
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+					userDetails, null, userDetails.getAuthorities());
+
+			authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+			SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+		}
+
+		filterChain.doFilter(request, response);
 	}
 
-	private String getJwtRequest(HttpServletRequest request) {
+	private String getJwtFromRequest(HttpServletRequest request) {
 
 		String bearerToken = request.getHeader("Authorization");
 
