2ef94cffc73750496159d0be90e812387f4fb886
kaustubhz <kaustubhz@cdac.in>
2020-12-04 22:20:14 +0530

    Added two end points for fetching all comments either by post id or username

--- RedditBackEnd/src/main/java/com/example/demo/controller/CommentsController.java
+++ RedditBackEnd/src/main/java/com/example/demo/controller/CommentsController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.CommentsDto;
+import com.example.demo.service.CommentService;
 
 import lombok.AllArgsConstructor;
 
@@ -14,9 +21,21 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CommentsController {
 
+	private final CommentService commentService;
+
 	@PostMapping
-	public void createComment(@RequestBody CommentsDto commen) {
-		
+	public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+		commentService.save(commentsDto);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
+	@GetMapping("/by-post/{postId}")
+	public ResponseEntity<List<CommentsDto>> getAllCommentsForPosts(@PathVariable Long postId) {
+		return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForPosts(postId));
+	}
+
+	@GetMapping("/by-user/{userName}")
+	public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@PathVariable String userName) {
+		return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForUser(userName));
+	}
 }
