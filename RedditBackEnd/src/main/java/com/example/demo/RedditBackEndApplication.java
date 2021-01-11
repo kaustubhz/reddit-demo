ffb86b1e1320254743fa66fc02c5328edd05f713
kaustubhz <kaustubhz@cdac.in>
2021-01-11 21:38:26 +0530

    Annotation for swagger

--- RedditBackEnd/src/main/java/com/example/demo/RedditBackEndApplication.java
+++ RedditBackEnd/src/main/java/com/example/demo/RedditBackEndApplication.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.example.demo.config.SwaggerConfig;
 
 @SpringBootApplication
 @EnableAsync
+//The following annotation will invoke spring fox at the start of application
+//It'll scan for all controllers and dto's to generate documentation for REST APIs
+@Import(SwaggerConfig.class)
 public class RedditBackEndApplication {
 
 	public static void main(String[] args) {
